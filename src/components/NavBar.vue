eee8c8e839d9d70fc0daa0cbef3a6976f902a30d
Xinghan <liuxinghan1998@gmail.com>
2020-04-18 19:13:49 +0800

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,31 +1,29 @@
 <template>
   <div class="fit row items-center bg-blue-grey-10">
-    <div class="col">
-      <q-list class="column items-center text-grey">
-        <q-item-label header class="col text-subtitle1">
-          BROWSE
-        </q-item-label>
- 
-        <q-item
-          clickable
-          v-ripple
-          exact
-          :to="link.path"
-          active-class="text-white text-weight-regular"
-          v-for="(link, index) in links"
-          :key="index"
-          class="col"
-        >
-          <q-item-section avatar>
-            <q-icon color="primary" :name="link.icon" />
-          </q-item-section>
+    <q-list class="col column items-center text-grey">
+      <q-item-label header class="col text-subtitle1">
+        BROWSE
+      </q-item-label>
 
-          <q-item-section class="text-h5">
-            {{link.title}}
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </div>
+      <q-item
+        clickable
+        v-ripple
+        exact
+        :to="link.path"
+        active-class="text-white text-weight-regular"
+        v-for="(link, index) in links"
+        :key="index"
+        class="col text-h5"
+      >
+        <q-item-section avatar>
+          <q-icon :name="link.icon" />
+        </q-item-section>
+
+        <q-item-section>
+          {{link.title}}
+        </q-item-section>
+      </q-item>
+    </q-list>
   </div>
 </template>
 
