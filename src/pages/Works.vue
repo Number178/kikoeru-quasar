6f3fa1a02717018ef389c349056433b5deb91182
Xinghan <liuxinghan1998@gmail.com>
2020-04-20 16:34:06 +0800

    Update Works.vue

--- src/pages/Works.vue
+++ src/pages/Works.vue
@@ -167,11 +167,8 @@ export default {
           }
         })
         .catch((error) => {
-          if (error.response) {
-            // 请求已发出，但服务器响应的状态码不在 2xx 范围内
-            if (error.response.status !== 401) {
-              this.showErrNotif(error.response.data.error || `${error.response.status} ${error.response.statusText}`)
-            }
+          if (error.response && error.response.status !== 401) {
+            this.showErrNotif(error.response.data.error || `${error.response.status} ${error.response.statusText}`)
           } else {
             this.showErrNotif(error.message || error)
           }
@@ -203,11 +200,8 @@ export default {
             this.pageTitle = pageTitle
           })
           .catch((error) => {
-            if (error.response) {
-              // 请求已发出，但服务器响应的状态码不在 2xx 范围内
-              if (error.response.status !== 401) {
-                this.showErrNotif(error.response.data.error || `${error.response.status} ${error.response.statusText}`)
-              }
+            if (error.response && error.response.status !== 401) {
+              this.showErrNotif(error.response.data.error || `${error.response.status} ${error.response.statusText}`)
             } else {
               this.showErrNotif(error.message || error)
             }
