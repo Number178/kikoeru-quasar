3713b202c48548d0268807e79f5ffabaf4359dbc
Xinghan <liuxinghan1998@gmail.com>
2020-04-20 21:43:45 +0800

    Update socket.io.js

--- src/boot/socket.io.js
+++ src/boot/socket.io.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueSocketIO from 'vue-socket.io'
 
 Vue.use(new VueSocketIO({
-  debug: true,
+  debug: false,
   connection: 'localhost:8888', // [调试环境使用]
   // connection: '', // [生成环境使用] 此项为空字符串时，默认将尝试连接到提供当前页面的主机
   options: {
