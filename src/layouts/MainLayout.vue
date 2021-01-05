13051724dcbb58d3dc80d6ecced3e7acc169cf0b
mason1900 <mw2010a@gmail.com>
2021-01-05 18:40:09 -0500

    回到顶部按钮

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -97,7 +97,9 @@
       <!-- <q-page padding> -->
         <router-view class="page-content" />
       <!-- </q-page> -->
-      
+        <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[18, 18]">
+          <q-btn fab icon="keyboard_arrow_up" color="accent" />
+        </q-page-scroller>
     </q-page-container>
 
     <q-footer class="q-pa-none">
