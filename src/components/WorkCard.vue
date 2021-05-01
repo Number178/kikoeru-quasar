4b2b7fc55ee9c1b2807700ef2c97ad1b3f23edee
mason1900 <mw2010a@gmail.com>
2021-05-01 06:57:49 -0400

    feat: "all age" label （close kikoeru-project/kikoeru-express#49)

--- src/components/WorkCard.vue
+++ src/components/WorkCard.vue
@@ -74,7 +74,8 @@
     <!-- 价格&售出数 -->
     <div v-show="metadata.title">
       <span class="q-mx-sm text-weight-medium text-h6 text-red">{{ metadata.price }} 日元</span>
-      售出数: {{ metadata.dl_count }}
+      <span>售出数: {{ metadata.dl_count }}</span>
+      <span v-if="!metadata.nsfw" class="q-mx-sm" style="background: #e6f7d6; color: #56842a">全年龄</span>
     </div>
 
     <!-- 标签 -->
