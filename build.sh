14a3f48120b4ea2967b76efbf76f62a2b66bfdb7
number18 <3137007658@qq.com>
2023-09-29 19:17:06 +0800

    feat: development script improve

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 npx quasar build -m pwa
+# npx quasar build -m spa
 
 echo copy to dist folder in local machine
-cp ./dist/pwa/* ../kikoeru-express/dist -rf
+rm ../kikoeru-express/dist/* -r
+cp ./dist/pwa/* ../kikoeru-express/dist -r
+# cp ./dist/spa/* ../kikoeru-express/dist -r
 echo copy finished
